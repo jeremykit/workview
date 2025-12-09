e413a2bf1ba9761b05b30196274b70c05c9c40a5
jeremykit <millzhang1@gmail.com>
2025-12-09 15:59:11 +0800

    Fix anthropic dependency version

---
+++ app/browser/selectors.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+"""
+Placeholder selectors for BOSS 直聘页面。根据实际页面结构可以调整。
+"""
+
+LIST_ITEM_SELECTOR = "div.job-card-wrapper"
+TITLE_SELECTOR = "span.job-name"
+COMPANY_SELECTOR = "div.company-name"
+SALARY_SELECTOR = "span.salary"
+CITY_SELECTOR = "span.workplace"
+DETAIL_LINK_SELECTOR = "a.card-link"
+JD_FULL_SELECTOR = "div.job-sec-text"
